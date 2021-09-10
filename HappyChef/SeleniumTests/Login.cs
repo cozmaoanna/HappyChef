49e37f5cf1c6f4eba658e76ad4e1d29d4466b93c
OannaCozma <cozmaoanna@yahoo.com>
2021-09-10 13:56:49 +0100

    moving to Azure SQL, database migration and connection string updated

---
+++ HappyChef/SeleniumTests/Login.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HappyChef.Client.SeleniumTests
+{
+    public class Login : BaseClass
+    {
+        [Test]
+        public void LoginMethod()
+        {
+            driver.Navigate().GoToUrl(URL);
+            driver.FindElementByCssSelector("a[href=authentication/login]");
+            wait.Until(x => x.FindElements(By.TagName("button")).FirstOrDefault(y => y.Text == "Sign In"));
+            var username = driver.FindElementByPartialLinkText("email");
+            username.SendKeys(UserName);
+            var password = driver.FindElementByPartialLinkText("password");
+            password.SendKeys(Password);
+            var loginbutton = driver.FindElementByClassName("btn");
+            loginbutton.Click();
+
+            wait.Until(x => x.FindElements(By.TagName("h4"))).FirstOrDefault(y => y.Text == HappyChefTestUsername);
+        }
+    }
+}
